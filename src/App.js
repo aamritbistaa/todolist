0836b0835a0e082590132d6931ed28932b34482b
Amrit Bista <aamritbistaa@gmail.com>
2023-02-20 17:59:49 +0545

    fc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
 import Todo from "./Pages/Todo";
 
 function App() {
   return (
     <div className="App">
-      {/* <Login />
-      <Register /> */}
       <Todo />
     </div>
   );
